c42ee7e73e0c0d8a485ae625c358290d8b0d0166
jeovan farias <jeovan.farias@sefin.ro.gov.br>
2021-09-07 09:51:13 -0400

    att

--- app/controllers/loja/home_controller.rb
+++ app/controllers/loja/home_controller.rb
@@ -1,3 +1,13 @@
 class Loja::HomeController < ApplicationController
   skip_before_action :authenticate_usuario!
+  layout 'loja'
+
+  def index
+    if params[:nome].present?
+      @produtos = Dashboard::Produto.where(" nome like :nome",{:nome => "%#{params[:nome]}%"}).all
+    else
+      @produtos = Dashboard::Produto.all
+    end
+    
+  end
 end
